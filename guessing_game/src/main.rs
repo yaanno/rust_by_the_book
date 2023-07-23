b5ccd7bf69c8aaa17038f39e8d6abfb60aadd02b
Janos Hardi <yaanno@gmail.com>
2023-07-23 12:41:40 +0200

    unneeded println

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -23,8 +23,6 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        println!("You guessed: {}", guess);
-
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue
